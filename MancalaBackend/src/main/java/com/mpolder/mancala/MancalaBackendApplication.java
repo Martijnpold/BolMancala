b4bc3b962bcd5ff06a98be0e61470b52cf4d01c7
Martijnpold <martijntje7@gmail.com>
2023-03-19 20:23:48 +0100

    init backend

---
+++ MancalaBackend/src/main/java/com/mpolder/mancala/MancalaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mpolder.mancala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancalaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancalaBackendApplication.class, args);
+	}
+
+}
