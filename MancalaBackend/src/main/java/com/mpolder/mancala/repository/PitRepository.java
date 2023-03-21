726c058ea9ddf584e12155d4d015f71dd9aff0e2
Martijnpold <martijntje7@gmail.com>
2023-03-21 13:37:46 +0100

    Modify repository inheritance to JpaRepository

--- MancalaBackend/src/main/java/com/mpolder/mancala/repository/PitRepository.java
+++ MancalaBackend/src/main/java/com/mpolder/mancala/repository/PitRepository.java
@@ -2,9 +2,13 @@ package com.mpolder.mancala.repository;
 
 import com.mpolder.mancala.model.Pit;
 import com.mpolder.mancala.model.idclass.PitId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface PitRepository extends CrudRepository<Pit, PitId> {
+public interface PitRepository extends JpaRepository<Pit, PitId> {
+    List<Pit> findAllByIdGameId(UUID gameId);
 }
--- MancalaBackend/src/main/java/com/mpolder/mancala/repository/PlayerRepository.java
+++ MancalaBackend/src/main/java/com/mpolder/mancala/repository/PlayerRepository.java
@@ -2,9 +2,13 @@ package com.mpolder.mancala.repository;
 
 import com.mpolder.mancala.model.Player;
 import com.mpolder.mancala.model.idclass.PlayerId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface PlayerRepository extends CrudRepository<Player, PlayerId> {
+public interface PlayerRepository extends JpaRepository<Player, PlayerId> {
+    List<Player> findAllByIdGameId(UUID gameId);
 }
