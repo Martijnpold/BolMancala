81bba68383cda11d57f661b3cf49a9984f4889ec
Martijnpold <martijntje7@gmail.com>
2023-03-28 13:40:31 +0200

    Write some frontend tests

--- MancalaFrontend/src/app/core/board/board.component.spec.ts
+++ MancalaFrontend/src/app/core/board/board.component.spec.ts
@@ -1,4 +1,4 @@
-import { MockBuilder, MockInstance, MockRender } from 'ng-mocks';
+import { MockBuilder, MockInstance, MockRender, ngMocks } from 'ng-mocks';
 import { AppModule } from 'src/app/app.module';
 import { Board } from 'src/app/model/board';
 import { Game } from 'src/app/model/game';
@@ -13,18 +13,18 @@ describe('BoardComponent', () => {
     game: fakeGame,
     pits: [
       { game: fakeGame, index: 0, marbles: 6 },
-      { game: fakeGame, index: 1, marbles: 6 },
-      { game: fakeGame, index: 2, marbles: 6 },
-      { game: fakeGame, index: 3, marbles: 6 },
-      { game: fakeGame, index: 4, marbles: 6 },
-      { game: fakeGame, index: 5, marbles: 6 },
+      { game: fakeGame, index: 1, marbles: 5 },
+      { game: fakeGame, index: 2, marbles: 4 },
+      { game: fakeGame, index: 3, marbles: 3 },
+      { game: fakeGame, index: 4, marbles: 2 },
+      { game: fakeGame, index: 5, marbles: 1 },
       { game: fakeGame, index: 6, marbles: 0 }, //End pit 1
       { game: fakeGame, index: 7, marbles: 6 },
-      { game: fakeGame, index: 8, marbles: 6 },
-      { game: fakeGame, index: 9, marbles: 6 },
-      { game: fakeGame, index: 10, marbles: 6 },
-      { game: fakeGame, index: 11, marbles: 6 },
-      { game: fakeGame, index: 12, marbles: 6 },
+      { game: fakeGame, index: 8, marbles: 5 },
+      { game: fakeGame, index: 9, marbles: 4 },
+      { game: fakeGame, index: 10, marbles: 3 },
+      { game: fakeGame, index: 11, marbles: 2 },
+      { game: fakeGame, index: 12, marbles: 1 },
       { game: fakeGame, index: 13, marbles: 0 }, //End pit 2
     ]
   }
@@ -41,5 +41,48 @@ describe('BoardComponent', () => {
     expect(fixture.point.componentInstance).toEqual(
       jasmine.any(BoardComponent),
     );
+  });
+
+  it('should render all pits', () => {
+    const fixture = MockRender(BoardComponent, {
+      board: fakeBoard
+    });
+
+    const pits = ngMocks.findAll('app-pit');
+    expect(pits.length).toEqual(14);
+  });
+
+  it('should pass in correct pit', () => {
+    const fixture = MockRender(BoardComponent, {
+      board: fakeBoard
+    });
+
+    const pits = ngMocks.findAll('app-pit');
+    expect(pits.length).toEqual(14);
+  });
+
+  it('should show buttons on playing side', () => {
+    const fixture = MockRender(BoardComponent, {
+      board: fakeBoard,
+      side: "BOTTOM"
+    });
+
+    const pits = ngMocks.findAll('app-pit');
+    for (let i = 0; i < 14; i++) {
+      let inst = pits[i].componentInstance;
+      let side = inst.pit.index >= 7 ? "BOTTOM" : "TOP"
+      expect(inst.showButton).withContext(`Checking value ${i}`).toEqual(fixture.componentInstance.side == side);
+    }
+  });
+
+  it('should emit an event on move', () => {
+    const fixture = MockRender(BoardComponent);
+    fixture.componentInstance.board = fakeBoard;
+    spyOn(fixture.componentInstance.onMove, 'emit')
+    fixture.detectChanges()
+
+    fixture.componentInstance.click(5)
+
+    expect(fixture.componentInstance.onMove.emit).toHaveBeenCalledOnceWith(5);
   });
 });
--- MancalaFrontend/src/app/core/pit/pit.component.spec.ts
+++ MancalaFrontend/src/app/core/pit/pit.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockInstance, MockBuilder, MockRender } from 'ng-mocks';
+import { MockInstance, MockBuilder, MockRender, ngMocks } from 'ng-mocks';
 import { AppModule } from 'src/app/app.module';
 import { Game } from 'src/app/model/game';
 import { PitComponent } from './pit.component';
@@ -15,11 +15,84 @@ describe('PitComponent', () => {
 
   it('should create', () => {
     const fixture = MockRender(PitComponent, {
-      pit: { game: fakeGame, index: 2, marbles: 5 }
+      pit: { game: fakeGame, index: 2, marbles: 5 },
     });
 
     expect(fixture.point.componentInstance).toEqual(
       jasmine.any(PitComponent),
     );
+  });
+
+  it('should display the marble count in a header', () => {
+    const fixture = MockRender(PitComponent, {
+      pit: { game: fakeGame, index: 2, marbles: 5 },
+    });
+
+    const header = ngMocks.find('h1');
+    expect(header.nativeElement.innerHTML).toEqual('5');
+  });
+
+  it('should not show any buttons when hasButtons is false', () => {
+    const fixture = MockRender(PitComponent, {
+      pit: { game: fakeGame, index: 2, marbles: 5 },
+      hasButton: false,
+      showButton: true,
+    });
+
+    const button = ngMocks.findAll('button');
+    expect(button.length).toBe(0);
+  });
+
+  it('should not show any buttons when showButtons is false', () => {
+    const fixture = MockRender(PitComponent, {
+      pit: { game: fakeGame, index: 2, marbles: 5 },
+      hasButton: true,
+      showButton: false,
+    });
+
+    const button = ngMocks.findAll('button');
+    expect(button.length).toBe(0);
+  });
+
+
+  it('should show a disabled button', () => {
+    const fixture = MockRender(PitComponent, {
+      pit: { game: fakeGame, index: 2, marbles: 5 },
+      hasButton: true,
+      showButton: true,
+      disabled: true,
+    });
+
+    const button = ngMocks.find('button');
+    expect(button).toBeTruthy();
+    expect(button.nativeElement.attributes.getNamedItem('ng-reflect-disabled')?.value).toBe('true');
+  });
+
+  it('should show an enabled button', () => {
+    const fixture = MockRender(PitComponent, {
+      pit: { game: fakeGame, index: 2, marbles: 5 },
+      hasButton: true,
+      showButton: true,
+      disabled: false,
+    });
+
+    const button = ngMocks.find('button')
+    expect(button).toBeTruthy();
+    expect(button.nativeElement.attributes.getNamedItem('ng-reflect-disabled')?.value).toBe('false');
+  });
+
+  it('should emit an event when clicked', () => {
+    const fixture = MockRender(PitComponent, {
+      pit: { game: fakeGame, index: 2, marbles: 5 },
+      hasButton: true,
+      showButton: true,
+      disabled: false,
+      onMove: jasmine.createSpy(),
+    });
+
+    const button = ngMocks.find('button');
+    expect(button).toBeTruthy();
+    ngMocks.click(button);
+    expect(fixture.componentInstance.onMove).toHaveBeenCalled();
   });
 });
